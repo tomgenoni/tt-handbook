2511cdc535cef4bbcbfa3fa32d9aedc767b256eb
Tom Genoni <tom@atomeye.com>
2018-07-03 14:50:46 -0700

    Renaming

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass');
+var gulp = require("gulp"),
+    sass = require("gulp-sass");
 
 function swallowError(error) {
-    console.log(error.message.toString())
-    this.emit('end')
+    console.log(error.message.toString());
+    this.emit("end");
 }
-    
-gulp.task('sass', function() {
-    return gulp.src("src/*.scss")
-        .pipe(sass({
-            includePaths: [
-                'node_modules'
-            ]
-        }))
+
+gulp.task("sass", function() {
+    return gulp
+        .src("src/**/*.scss")
+        .pipe(
+            sass({
+                includePaths: ["node_modules"]
+            })
+        )
         .pipe(gulp.dest("dist"));
 });
-    
+
+// cssjson dist/utils.css -o dist/docs/utils.json
