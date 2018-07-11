c69dbdae220f2479d7125750d83183c836cb31b0
Tom Genoni <tom@atomeye.com>
2018-07-11 13:41:48 -0700

    Use handlebars

--- code-examples.js
+++ code-examples.js
@@ -4,11 +4,19 @@ const fs = require("fs-extra"),
 
 const examplesPath = "./docs/examples/";
 const exampleTemplate = "./docs/views/example.handlebars";
+const exampleIndexTemplate = "./docs/views/example-index.handlebars";
 const dirExamples = "./dist/examples/";
+const examplesArr = [];
 
 function highlight(lang, sourceCode) {
     const language = Prism.languages[lang] || Prism.languages.autoit;
     return Prism.highlight(sourceCode, language);
+}
+
+function buildIndexData(file) {
+    examplesArr.push({
+        title: file
+    });
 }
 
 function getCode(file) {
@@ -31,15 +39,21 @@ function getCode(file) {
     );
 }
 
-// Move the index.html
-fs.copy("./docs/examples.html", "./dist/examples.html");
-
 fs.readdir(examplesPath, (err, files) => {
     files.forEach(file => {
         if (!file.startsWith(".")) {
+            // Create dir and get file code
             fs.ensureDir(dirExamples + file, err => {
                 getCode(file);
             });
+
+            buildIndexData(file);
         }
     });
+
+    let source = fs.readFileSync(exampleIndexTemplate, "utf8");
+    let template = hb.compile(source);
+    let result = template(examplesArr);
+
+    fs.writeFile("./dist/examples.html", result, function() {});
 });
