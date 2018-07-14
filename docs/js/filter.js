79df56de6bb426ac9ac092ce49a8e37532589573
Tom Genoni <tom@atomeye.com>
2018-07-14 01:17:20 -0700

    Add seek

--- docs/js/filter.js
+++ docs/js/filter.js
@@ -1,19 +1,27 @@
-const dataClass = document.querySelectorAll(".filter-class");
-const dataProperty = document.querySelectorAll(".filter-property");
-const dataValue = document.querySelectorAll(".filter-value");
+// Elements
+// --------------------------------
 
-const body = document.querySelector("body");
+const data = {
+    class    : document.querySelectorAll(".filter-class"),
+    property : document.querySelectorAll(".filter-property"),
+    value    : document.querySelectorAll(".filter-value")
+}
+const filter = {
+    input : document.querySelector("#filter"),
+    type  : document.querySelector("#filter-type"),
+    seek  : document.querySelector("#filter-seek")
+}
 
-const filterInput = document.querySelector("#filter");
-const filterSelect = document.querySelector("#filter-type");
+const body   = document.querySelector("body");
 const enties = document.querySelectorAll(".entry");
+
+// Functions
+// --------------------------------
 
 function purgeMatches() {
     const hits = document.querySelectorAll(".hit");
     enties.forEach(function (i) {
-        i.classList.remove("match-class");
-        i.classList.remove("match-property");
-        i.classList.remove("match-value");
+        i.classList.remove("match-class", "match-property", "match-value");
     });
     hits.forEach(function (i) {
         i.classList.remove("hit");
@@ -21,42 +29,61 @@ function purgeMatches() {
     body.classList.remove("filter-active");
 }
 
-function addFilterClass(arr, term, className) {
+function addFilterClass(arr, term, seek, className) {
     arr.forEach(function (i) {
-        let classStr = i.innerHTML;
-        let entry = i.closest(".entry");
+        let classStr = i.textContent;
+        let entry    = i.closest(".entry");
 
-        if (classStr.startsWith(term)) {
-            entry.classList.add(className);
-            i.classList.add("hit");
+        if (seek === "starts") {
+            if (classStr.startsWith(term)) {
+                entry.classList.add(className);
+                i.classList.add("hit");
+            }
+        } else {
+            if (classStr.includes(term)) {
+                entry.classList.add(className);
+                i.classList.add("hit");
+            }
         }
     });
 }
 
-function filterClasses(term, type) {
+function filterClasses(term, type, seek) {
     purgeMatches();
 
-    if (term != "") {
+    if (term !== "") {
         body.classList.add("filter-active");
 
         if (type == "class") {
-            addFilterClass(dataClass, term, "match-class");
+            addFilterClass(data.class, term, seek, "match-class");
         } else if (type == "property") {
-            addFilterClass(dataProperty, term, "match-property");
+            addFilterClass(data.property, term, seek, "match-property");
         } else {
-            addFilterClass(dataValue, term, "match-value");
+            addFilterClass(data.value, term, seek, "match-value");
         }
     }
 }
 
-filterInput.addEventListener("keyup", function () {
+// Event listeners
+// --------------------------------
+
+filter.input.addEventListener("keyup", function () {
     let term = this.value;
-    let type = filterSelect.value;
-    filterClasses(term, type);
+    let type = filter.type.value;
+    let seek = filter.seek.value;
+    filterClasses(term, type, seek);
 })
 
-filterSelect.addEventListener("change", function () {
-    let term = filterInput.value;
+filter.type.addEventListener("change", function () {
+    let term = filter.input.value;
     let type = this.value;
-    filterClasses(term, type);
+    let seek = filter.seek.value;
+    filterClasses(term, type, seek);
+});
+
+filter.seek.addEventListener("change", function () {
+    let term = filter.input.value;
+    let type = filter.type.value;
+    let seek = this.value;
+    filterClasses(term, type, seek);
 })
