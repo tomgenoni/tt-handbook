a43e34e1d5f44716f684c0e6795104354aab06b0
Tom Genoni <tom@atomeye.com>
2018-07-16 14:01:21 -0700

    Clear filter

--- docs/js/filter.js
+++ docs/js/filter.js
@@ -2,25 +2,25 @@
 // --------------------------------
 
 const data = {
-    class    : document.querySelectorAll(".filter-class"),
-    property : document.querySelectorAll(".filter-property"),
-    value    : document.querySelectorAll(".filter-value")
-}
+    class: document.querySelectorAll(".filter-class"),
+    property: document.querySelectorAll(".filter-property"),
+    value: document.querySelectorAll(".filter-value")
+};
 const filter = {
-    input : document.querySelector("#filter"),
-    type  : document.querySelector("#filter-type"),
-    seek  : document.querySelector("#filter-seek")
-}
-
-const body   = document.querySelector("body");
-const enties = document.querySelectorAll(".entry");
+    input: document.querySelector("#filter"),
+    type: document.querySelector("#filter-type"),
+    seek: document.querySelector("#filter-seek"),
+    clear: document.querySelector("#filter-clear")
+};
 
 const noHit = {
     type: document.querySelector("#no-hit-type"),
     term: document.querySelector("#no-hit-term"),
     seek: document.querySelector("#no-hit-seek")
-}; 
+};
 
+const body = document.querySelector("body");
+const enties = document.querySelectorAll(".entry");
 
 // Functions
 // --------------------------------
@@ -29,12 +29,18 @@ function purgeMatches() {
     const hits = document.querySelectorAll(".hit");
     body.classList.remove("filter-active");
 
-    hits.forEach(function (i) {
+    hits.forEach(function(i) {
         i.classList.remove("hit");
     });
-    enties.forEach(function (i) {
+    enties.forEach(function(i) {
         i.classList.remove("match-class", "match-property", "match-value");
     });
+}
+
+function clearSearch() {
+    body.classList.remove("filter-active");
+    filter.input.value = "";
+    filter.input.focus();
 }
 
 function highlightType(type) {
@@ -55,19 +61,19 @@ function hitTest(type, term, seek) {
 }
 
 function addFilterClass(arr, type, term, seek, className) {
-    arr.forEach(function (i) {
+    arr.forEach(function(i) {
         let classStr = i.textContent;
-        let entry    = i.closest(".entry");
+        let entry = i.closest(".entry");
 
         if (seek === "includes") {
             if (classStr.includes(term)) {
-              entry.classList.add(className);
-              i.classList.add("hit");
+                entry.classList.add(className);
+                i.classList.add("hit");
             }
         } else {
             if (classStr.startsWith(term)) {
-              entry.classList.add(className);
-              i.classList.add("hit");
+                entry.classList.add(className);
+                i.classList.add("hit");
             }
         }
     });
@@ -98,18 +104,31 @@ function filterClasses() {
 // Event listeners
 // --------------------------------
 
-filter.input.addEventListener("keyup", function () {
-    filterClasses();
-})
-
-filter.type.addEventListener("change", function () {
+filter.input.addEventListener("keyup", function() {
     filterClasses();
 });
 
-filter.seek.addEventListener("change", function () {
+filter.type.addEventListener("change", function() {
     filterClasses();
-})
+});
 
-window.onload = function () {
+filter.seek.addEventListener("change", function() {
+    filterClasses();
+});
+
+window.onload = function() {
+    filter.input.focus();
     filterClasses();
 };
+
+// Clear term input
+filter.clear.addEventListener("click", function() {
+    clearSearch();
+});
+
+// Escape key clears term input
+body.addEventListener("keyup", function(e) {
+    if (e.which == 27) {
+        clearSearch();
+    }
+});
