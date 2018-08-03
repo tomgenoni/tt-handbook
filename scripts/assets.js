61d77f2ec4578c7a766c45f83edf5b834c727242
Tom Genoni <tom@atomeye.com>
2018-08-03 10:50:46 -0700

    Add playground

--- scripts/assets.js
+++ scripts/assets.js
@@ -6,26 +6,36 @@ fs.copy("./docs/js", "./dist/js", err => {
     if (err) return console.error(err);
 });
 
+fs.copy("./docs/playground.html.zip", "./dist/playground.html.zip", err => {
+    if (err) return console.error(err);
+});
+
 // Compile CSS
-fs.ensureDir('./dist/css', err => {
+fs.ensureDir("./dist/css", err => {
+    sass.render(
+        {
+            file: "./docs/scss/tui.scss"
+        },
+        function(error, result) {
+            fs.writeFile("./dist/css/tui.css", result.css, function() {});
+        }
+    );
 
-    sass.render({
-        file: "./docs/scss/tui.scss"
-    }, function (error, result) {
-        fs.writeFile("./dist/css/tui.css", result.css, function () {});
-    });
+    sass.render(
+        {
+            file: "./docs/scss/utils.scss"
+        },
+        function(error, result) {
+            fs.writeFile("./dist/css/utils.css", result.css, function() {});
+        }
+    );
 
-    sass.render({
-        file: "./docs/scss/utils.scss"
-    }, function (error, result) {
-        fs.writeFile("./dist/css/utils.css", result.css, function () {});
-    });
-
-    sass.render({
-        file: "./docs/scss/examples.scss"
-    }, function (error, result) {
-        fs.writeFile("./dist/css/examples.css", result.css, function () {});
-    });
-})
-
-
+    sass.render(
+        {
+            file: "./docs/scss/examples.scss"
+        },
+        function(error, result) {
+            fs.writeFile("./dist/css/examples.css", result.css, function() {});
+        }
+    );
+});
